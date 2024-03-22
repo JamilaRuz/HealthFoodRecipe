c50310155dc8b0d2327a086debc0d9c324bd14a2
Jamila <j.ruzimetova@gmail.com>
2024-03-22 16:09:53 -0400

    - UI Changes to Category list - Card view elements were added - Horizontal carousel

---
+++ HealthyFoodRecipe/Views/CardView.swift
@@ -0,0 +1,45 @@
+//
+//  CardView.swift
+//  HealthyFoodRecipe
+//
+//  Created by Jamila Ruzimetova on 3/22/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct CardView: View {
+    let category: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image("dish")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: 200)
+            
+            VStack {
+                Text(category)
+                    .font(.headline)
+                    .minimumScaleFactor(0.1)
+                    .foregroundColor(.primary)
+                
+                Text("7 favourites")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                
+            }
+            .frame(maxWidth: .infinity, minHeight: 70)
+            .background(Color.white) // Set the background color
+        }
+        .cornerRadius(10) // Add rounded corners
+        .shadow(radius: 5) // Add a subtle shadow
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(category: "Salads and dressings")
+            .previewLayout(.sizeThatFits)
+    }
+}
--- HealthyFoodRecipe/Views/CategoryListView.swift
+++ HealthyFoodRecipe/Views/CategoryListView.swift
@@ -12,18 +12,41 @@ struct CategoryListView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var menuItems: [MenuItem]
     
+    let images = ["dessert", "dish", "garnish", "meat", "soup"]
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
         NavigationStack {
-            VStack() {
-                ForEach([String](Set(recipes.map{$0.Category!})), id: \.self) { category in
-                    NavigationLink(destination: RecipeListView(category: category)) {
-                        Text(category)
+            VStack(alignment: .leading) {
+                ScrollView(.horizontal, showsIndicators: true) {
+                    HStack(spacing: 0) {
+                        ForEach(images, id: \.self) { image in
+                            Image(image)
+                                .scaledToFill()
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .cornerRadius(10)
+                .shadow(radius: 10)
+                
+                Text("Choose a category")
+                    .font(.title2)
+                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach([String](Set(recipes.map{$0.Category!})), id: \.self) { category in
+                            NavigationLink(destination: RecipeListView(category: category)) {
+                                CardView(category: category)
+                            }
+                        }
                     }
                 }
             }
-            .frame(width: 150, height: 100)
-            .cornerRadius(3.0)
-            .navigationTitle("Categories")
+            .padding()
+            .navigationTitle("Our dishes")
         }
     }
 }
