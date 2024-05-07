e697c2af978e3acf23e803c6e20c7096b0fdd331
Jamila <j.ruzimetova@gmail.com>
2024-05-07 13:03:19 -0400

    - Menu View UI changes

---
+++ HealthyFoodRecipe/Views/MenuDayView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuDayView.swift
+//  HealthyFoodRecipe
+//
+//  Created by Jamila Ruzimetova on 4/30/24.
+//
+
+import SwiftUI
+
+struct MenuDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuDayView()
+}
